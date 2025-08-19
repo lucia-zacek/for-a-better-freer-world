30dd454597d7cf1ef59f514d003a8492dc9fb098
lucia-zacek <luciazacek@gmail.com>
2025-08-18 21:34:12 -0400

    Initial placeholders & styles

---
+++ app/src/welcome/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.scss";
+
+const Welcome = () => {
+  return (
+    <div className="welcome-container">
+      <span className="welcome-title">
+        Welcome!
+      </span>
+      <span className="welcome-message">
+        Hi! Placeholder message. we can add text here!
+      </span>
+    </div>
+  );
+};
+
+export default Welcome;
