0c9fb804cd505725e820c10b0052fd8f6b64817d
lucia-zacek <luciazacek@gmail.com>
2025-09-06 16:45:12 -0400

    Updates to messaging

--- app/src/particles/ParticlesBackground.tsx
+++ app/src/particles/ParticlesBackground.tsx
@@ -1,6 +1,6 @@
 import { FC } from "react";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadFountainPreset } from "@tsparticles/preset-fountain";
+import { loadLinksPreset } from "@tsparticles/preset-links";
 import { useEffect, useState } from "react";
 
 const ParticlesBackground: FC = () => {
@@ -9,15 +9,15 @@ const ParticlesBackground: FC = () => {
   useEffect(() => {
     // load the engine once on mount
     initParticlesEngine(async (engine) => {
-      await loadFountainPreset(engine);
+      await loadLinksPreset(engine);
     }).then(() => {
       setInit(true);
     });
   }, []);
 
   const options: any = {
-    preset: "fountain",
-    background: { color: { value: "#ffffffff" } },
+    preset: "links",
+    background: { color: { value: "#3a4885ff" } },
   };
 
   return init ? <Particles id="tsparticles" options={options} /> : null;
--- app/src/welcome/index.tsx
+++ app/src/welcome/index.tsx
@@ -1,13 +1,48 @@
 import "./styles.scss";
+import familyPhoto from '../assets/images/family_photo.jpg';
 
 const Welcome = () => {
   return (
     <div className="welcome-container">
       <span className="welcome-title">
-        Happy Birthday!
+        Mission & Vision
       </span>
-      <span className="welcome-message">
-        Hi! This is a placeholder message, we can add text here!
+      <div>
+        <span className="welcome-message">
+          A place where your ideas and choices matter,
+          <br />
+          where a like-minded community
+          <br />
+          thrives on collaboration,
+          <br />
+          not exploitation.
+          <br /> <br />
+          Here, powerful tools are shared,
+          <br />
+          not hoarded;
+          <br />
+          you are free to build, improve, and innovate
+          <br />
+          without gatekeepers draining progress for profit.
+          <br /> <br />
+          It is not just a vision,
+          <br />
+          it is a tool against enshitification
+          <br />
+          and a stand for a 
+          {' '}
+        </span>
+        <span className="italic welcome-message">
+           better freer world
+        </span>
+        <span className="welcome-message">
+          <br />
+          we create together.
+        </span>
+      </div>
+      <img src={familyPhoto} className="family-photo" alt="Family" />
+      <span className="welcome-subtext">
+        Be The Change You Want To See In The World!
       </span>
     </div>
   );
